93f682a8036096c969b8f3ff21954e43b7cfdd06
kim8510 <kimlytea7@gmail.com>
2018-11-11 00:24:03 +0100

    Projet_Cloud_Games

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
 // CAROUSEL
-$(document).ready(function() {
+$(document).ready(function() { // service in module ng
   $("#myCarousel").on("slide.bs.carousel", function(e) {
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 3;
-    var totalItems = $(".carousel-item").length;
+    var $e = $(e.relatedTarget); // variable cible
+    var idx = $e.index(); // variable index
+    var itemsPerSlide = 3; // variable nombre d'items par slide
+    var totalItems = $(".carousel-item").length; //  variable longueur totalItems
 
-    if (idx >= totalItems - (itemsPerSlide - 1)) {
-      var it = itemsPerSlide - (totalItems - idx);
-      for (var i = 0; i < it; i++) {
+// condition if for, if else
+    if (idx >= totalItems - (itemsPerSlide - 1)) { // affiche d'un item en vue
+      var it = itemsPerSlide - (totalItems - idx); // valeur par soustraction de la variable it
+      for (var i = 0; i < it; i++) {  // incrémentation
         // append slides to end
-        if (e.direction == "left") {
+        if (e.direction == "left") { // slide des items strictement à droite
           $(".carousel-item")
             .eq(i)
             .appendTo(".carousel-inner");
-        } else {
+        } else { // sinon afficher tout les items
           $(".carousel-item")
             .eq(0)
             .appendTo($(this).find(".carousel-inner"));
